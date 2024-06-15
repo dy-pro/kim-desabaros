d412e9468099ea1350191392db6e7cee0b61215c
SMarsaDewo <satriamarsadewo@gmail.com>
2024-06-16 00:53:37 +0700

    readme

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('role')->default('user');
+            $table->string('whatsapp')->nullable();
+            $table->text('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
