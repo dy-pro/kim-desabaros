d34c5b42ec74a2681aa34124cdb73ba53551da33
SMarsaDewo <satriamarsadewo@gmail.com>
2024-06-19 13:16:49 +0700

    create database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->text('address')->nullable();
             $table->string('whatsapp')->nullable();
             $table->string('password');
