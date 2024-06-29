1138c07e6bf140ca51dacbb7a360d540011e9248
Ardyan Satya <ardyan.satya@gmail.com>
2024-06-29 23:52:30 +0700

    Rename category to categories in DinamisController

--- app/Http/Controllers/DinamisController.php
+++ app/Http/Controllers/DinamisController.php
@@ -15,11 +15,11 @@ class DinamisController extends Controller
         
         
         ->leftJoin('users', 'products.id_user', '=', 'users.id')
-        ->leftJoin('category', 'products.id_category', '=', 'category.id')
-        ->select('products.image as product_image','products.name as product_name', 'products.*', 'users.*', 'category.title as category_title','category.*')
+        ->leftJoin('categories', 'products.id_category', '=', 'categories.id')
+        ->select('products.image as product_image','products.name as product_name', 'products.*', 'users.*', 'categories.title as categories_title','categories.*')
         ->get();
-        $category=Category::all();
-        return view('pages.user.dinamis.home', ['products'=>$product, 'categories' => $category]);
+        $categories=Category::all();
+        return view('pages.user.dinamis.home', ['products'=>$product, 'categories' => $categories]);
     }
     public function about(){
         return view('pages.user.dinamis.about');
